3b9d4c8536fd182dac817f6308bc5cd17e2ebb4f
Anthony Arroyo <anthony.aarroy2@gmail.com>
2020-05-09 15:05:05 -0500

    deploy

---
+++ client/build/precache-manifest.7166209ae0ee5a695342989fb546aa87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9048e2240358aefe86e",
+    "url": "/static/css/main.c31fa72d.chunk.css"
+  },
+  {
+    "revision": "e9048e2240358aefe86e",
+    "url": "/static/js/main.4402fd07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2cafbc6c619a325ee6a",
+    "url": "/static/js/2.d882187c.chunk.js"
+  },
+  {
+    "revision": "c8cbbdaceef2b215006047bae969313e",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://aroyals22:Aa278522@ds139322.mlab.com:39322/heroku_frmhqvp1");
 
 // Start the API server
 app.listen(PORT, function() {
